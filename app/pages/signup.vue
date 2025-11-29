6b0c219f285e96e29a9357739ec0d8e20b44e35a
NinjaVass-C <connorvassinfo@gmail.com>
2025-11-29 12:54:10 -0500

    Front end working

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -3,7 +3,10 @@
   <h1>Welcome to Dropadox</h1>
   <h2>Sign up Now</h2>
     <div class="signup flex">
+      <p v-if="submissionError">{{submissionError}}</p>
 
+      <input v-model="state.name" placeholder="Name" required />
+      <p v-if="nameError">{{nameError}}</p>
       <input v-model="state.email" placeholder="email"/>
       <p v-if="emailError">{{ emailError }}</p>
       <input v-model="state.password" placeholder="password"/>
@@ -14,15 +17,21 @@
 </template>
 <script lang="ts" setup>
   const state = reactive({
+    name: '',
     email: '',
     password: '',
   })
   const passwordError = ref();
   const emailError = ref();
+  const nameError = ref();
+  const submissionError = ref();
+  const router = useRouter();
 
-  function signUp() {
+  async function signUp() {
       emailError.value = false
       passwordError.value = false
+      nameError.value = false
+      submissionError.value = false
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailRegex.test(state.email)) {
           emailError.value = 'Please enter a valid email';
@@ -30,9 +39,27 @@
       if (state.password.length < 6) {
         passwordError.value = 'Please enter a password';
       }
+      if (state.name.length < 1) {
+        nameError.value = 'Please enter a name';
+      }
 
-      if (!emailError.value && !passwordError.value) {
+      if (!emailError.value && !passwordError.value && !nameError.value) {
         // add api connection here
+        try {
+          const res = await $fetch('api/register', {
+            method: 'POST',
+            body: {
+              username: state.name,
+              email: state.email,
+              password: state.password,
+            }
+          })
+          router.push('/login')
+        } catch (error) {
+           submissionError.value = "Unable to register user, please try again"
+        }
+
+
       }
   }
 
